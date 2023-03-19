1a005bd9a3ea18f5e2c6fecaed70c9a33a7e5f29
Aashish2712 <aashishrathod2506@gmail.com>
2023-03-19 11:37:31 +0530

    After presentation with all components

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ h1{
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="images/c1.jpg" class="d-block w-100" alt="...">
+                <img src="images/c2.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>First slide label</h5>
                     <p>Some representative placeholder content for the first slide.</p>
@@ -202,88 +202,27 @@ h1{
                 <!-- End Column 4 -->
 
             </div>
-            <div class="row">
-
-                <!-- Start Column 1 -->
-                <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
-                    <h2 class="mb-4 section-title">Crafted with excellent material.</h2>
-                    <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-                        vulputate velit imperdiet dolor tempor tristique. </p>
-                    <p><a href="shop.html" class="mbtn">Explore</a></p>
-                </div>
-                <!-- End Column 1 -->
-
-                <!-- Start Column 2 -->
-                <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                    <a class="product-item" href="cart.html">
-                        <img src="https://source.unsplash.com/500x400/?new television"
-                            class="img-fluid product-thumbnail">
-                        <h3 class="product-title">Nordic Chair</h3>
-                        <strong class="product-price">$50.00</strong>
-
-                        <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
-                        </span>
-                    </a>
-                </div>
-                <!-- End Column 2 -->
-
-                <!-- Start Column 3 -->
-                <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                    <a class="product-item" href="cart.html">
-                        <img src="https://source.unsplash.com/500x400/?new television"
-                            class="img-fluid product-thumbnail">
-                        <h3 class="product-title">Kruzo Aero Chair</h3>
-                        <strong class="product-price">$78.00</strong>
-
-                        <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
-                        </span>
-                    </a>
-                </div>
-                <!-- End Column 3 -->
-
-                <!-- Start Column 4 -->
-                <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                    <a class="product-item" href="cart.html">
-                        <img src="https://source.unsplash.com/500x400/?new television"
-                            class="img-fluid product-thumbnail">
-                        <h3 class="product-title">Ergonomic Chair</h3>
-                        <strong class="product-price">$43.00</strong>
-
-                        <span class="icon-cross">
-                            <img src="images/cross.svg" class="img-fluid">
-                        </span>
-                    </a>
-                </div>
-                <!-- End Column 4 -->
-
-            </div>
         </div>
-    </div>
-    <!-- End Product Section -->
 
 
 
 
+        <?php require 'parts/footer_.php'; ?>
 
+        <!-- Optional JavaScript; choose one of the two! -->
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+        </script>
 
-    <?php require 'parts/footer_.php'; ?>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
--- parts/header_.php
+++ parts/header_.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <nav class="navbar navbar-expand-lg " data-bs-theme="dark" style="background-color:#3b5d50;">
-  <a class="navbar-brand ml-3 logo" href="#">Lucky Electric</a>
+  <a class="navbar-brand ml-3 logo" href="index.php">Lucky Electric</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -9,7 +9,7 @@ echo '
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link  navcontents" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link  navcontents" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link navcontents" href="products.php">Products</a>
@@ -27,21 +27,21 @@ echo '
             Services
           </a>
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Our servises</a>
-            <a class="dropdown-item" href="#">About us</a>
-            <a class="dropdown-item" href="#">Contact us</a>
+            <a class="dropdown-item" href="services.php">Our services</a>
+            <a class="dropdown-item" href="about.php">About us</a>
+            <a class="dropdown-item" href="contact.php">Contact us</a>
             
           </div>
           
         </li> 
         <li class="nav-item">
-         <a class="nav-link navcontents" href="#"> Login</a>
+         <a class="nav-link navcontents" href="log.php"> <img class="" src="images/user.svg"></a>
        </li>
-       <li><a class="nav-link navcontents" href="#"><img src="images/user.svg"></a></li>
-						<li><a class="nav-link navcontents" href="cart.html"><img src="images/cart.svg"></a></li>
+       <!-- <li><a class="nav-link navcontents" href="#"></a></li> -->
+						<li><a class="nav-link navcontents" href="cart.php"><img src="images/cart.svg"></a></li>
     
     </ul>
-    <form class="form-inline my-2 my-lg-0 ">
+    <form class="form-inline my-2 my-lg-0 " action="products.php">
       <input class="form-control mr-sm-2 navform ml-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-warning mr-2" type="submit">Search</button>
     </form>
